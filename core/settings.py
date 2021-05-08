76f4abc986f6951c9d217219f8d048b64b2f4981
Rohan Singh <singhrohanss007@gmail.com>
2021-05-08 17:05:24 +0530

    static4

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$7-^axyzobgdqekyll#a0@)hl8qt^0r+#-n1ikch-xvb0*6(h-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','holagrid.herokuapp.com']
 
