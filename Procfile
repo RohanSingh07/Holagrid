eea49a84158b413f5ccb4fef528386137de7b362
Rohan Singh <singhrohanss007@gmail.com>
2021-05-10 03:21:19 +0530

    socket12

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn core.wsgi --log-file 
-web: daphne core.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+web: daphne core.asgi
--- core/asgi.py
+++ core/asgi.py
@@ -10,9 +10,8 @@ import django
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.layers import get_channel_layer
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 application = get_asgi_application()
-channel_layer = get_channel_layer()
